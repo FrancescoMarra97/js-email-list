7f3140886dfead16f8bb9ac0bdb8a258a05d4ea4
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-17 15:57:11 +0200

    generate 10 email

--- main.js
+++ main.js
@@ -3,14 +3,29 @@ Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api
 e stamparli in pagina all'interno di una lista.
  */
 
-fetch("https://flynn.boolean.careers/exercises/api/random/mail")
-.then(response => response.json())
-.then(data =>{
-    console.log(data);
+// da qui genero l'email
+/* fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+    })
+    .catch(error => {
+        console.error(error);
+    }); */
+
+emailList=[];
+
+for (let i = 0; i < 10; i++) {
+    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+    })
+    .catch(error => {
+        console.error(error);
+    });
     
-})
-.catch(error => {
-    console.error(error);
     
-    
-});
+}
